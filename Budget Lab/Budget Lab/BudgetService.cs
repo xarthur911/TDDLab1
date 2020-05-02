d319e541fad94c621ce700933da15fbd5ff535cf
ArthurLin <arthurlin@91app.com>
2020-05-02 16:44:28 +0800

    跨月

--- Budget Lab/Budget Lab/BudgetService.cs
+++ Budget Lab/Budget Lab/BudgetService.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.CodeDom;
 using System.Linq;
-using System.Runtime.InteropServices;
 
 namespace Budget_Lab
 {
@@ -15,26 +15,61 @@ namespace Budget_Lab
         public decimal Query(DateTime start, DateTime end)
         {
             var allAmount = this._budgetRepo.GetAll();
-            var diffDays = end.Subtract(start).TotalDays;
+            var diffDays = end.Subtract(start).TotalDays+1;
+            var diffMonth = end.Year * 12 + end.Month - (start.Year * 12 + start.Month) + 1;
+            var startMonthDays = DateTime.DaysInMonth(start.Year, start.Month);
+            var endMonthDays = DateTime.DaysInMonth(end.Year, end.Month);
+            var startAmount = allAmount
+                .FirstOrDefault(i => i.YearMonth == start.ToString("yyyyMM"))
+                ?.Amount ?? 0;
+            var endAmount = allAmount
+                .FirstOrDefault(i => i.YearMonth == end.ToString("yyyyMM"))
+                ?.Amount ?? 0;
+
+            decimal startOneDay = startAmount / startMonthDays;
+            decimal endOneDay = endAmount / endMonthDays;
 
             //// end < start
-            if (diffDays < 0)
+            if (diffDays < 1)
             {
                 return 0;
             }
 
             //// 當天
-            if (diffDays == 0)
+            if (diffDays == 1)
             {
-                var monthDays = DateTime.DaysInMonth(start.Year, start.Month);
-                var amount = allAmount
-                    .FirstOrDefault(i => i.YearMonth == start.ToString("yyyyMM"))
-                    ?.Amount??0;
-                return amount / monthDays;
+                return startOneDay;
             }
 
-            //// 當月超過1日
-            if()
+
+            if (diffMonth<2)
+            {
+                //// 當月超過1日
+                return (decimal)(diffDays) * startOneDay;
+            }
+            //// 跨2月
+            else if (diffMonth == 2)
+            {
+                var s = (startMonthDays - start.Day + 1) * startOneDay;
+                var e = end.Day * startOneDay;
+                return s + e;
+            }
+            //// 跨3個月以上
+            else
+            {
+                var s = (startMonthDays - start.Day + 1) * startOneDay;
+                var e = end.Day * startOneDay;
+                var tmpMid = (decimal) 0;
+                for (var i = 1; i < diffMonth-2; i++)
+                {
+                    var midAmount = allAmount
+                        .FirstOrDefault(j => j.YearMonth == start.AddMonths(i).ToString("yyyyMM"))
+                        ?.Amount ?? 0;
+                    tmpMid += midAmount;
+                }
+
+                return s + tmpMid + e;
+            }
 
             return 0;
         }
