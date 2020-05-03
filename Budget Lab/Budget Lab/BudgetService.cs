9e02b9b41dfcd07f7cdfc1dc3bee602e7293794e
Joey Chen <jay.hatelove@gmail.com>
2020-05-03 11:08:31 +0800

    [refactor]clean up

--- Budget Lab/Budget Lab/BudgetService.cs
+++ Budget Lab/Budget Lab/BudgetService.cs
@@ -1,6 +1,9 @@
-﻿using System;
-using System.CodeDom;
+﻿#region
+
+using System;
 using System.Linq;
+
+#endregion
 
 namespace Budget_Lab
 {
@@ -10,24 +13,20 @@ namespace Budget_Lab
 
         public BudgetService(IBudgetRepo budgetRepo)
         {
-            this._budgetRepo = budgetRepo;
+            _budgetRepo = budgetRepo;
         }
 
         public decimal Query(DateTime start, DateTime end)
         {
-            if (end < start)
-            {
-                return 0;
-            }
+            if (end < start) return 0;
 
             var budgets = _budgetRepo.GetAll();
 
             var firstBudget = budgets
                 .FirstOrDefault(i => i.YearMonth == start.ToString("yyyyMM"));
 
-            int amountOfStartBudget = 0;
-            decimal dailyAmountOfStart = 0m;
-            int daysOfStartBudget = 0;
+            var dailyAmountOfStart = 0m;
+            var daysOfStartBudget = 0;
             if (firstBudget != null)
             {
                 daysOfStartBudget = firstBudget.Days();
@@ -38,31 +37,27 @@ namespace Budget_Lab
             var amountOfEndBudget = budgets
                                     .FirstOrDefault(i => i.YearMonth == end.ToString("yyyyMM"))
                                     ?.Amount ?? 0;
-            decimal dailyAmountOfEnd = (decimal) amountOfEndBudget / daysOfEndBudget;
+            var dailyAmountOfEnd = (decimal) amountOfEndBudget / daysOfEndBudget;
 
             if (start.ToString("yyyyMM") == end.ToString("yyyyMM"))
             {
                 var intervalDays = (end - start).Days + 1;
                 return intervalDays * dailyAmountOfStart;
             }
-            else
-            {
-                var s = (daysOfStartBudget - start.Day + 1) * dailyAmountOfStart;
-                var e = end.Day * dailyAmountOfEnd;
-                var tmpMid = (decimal) 0;
-                var diffMonth = end.Year * 12 + end.Month - (start.Year * 12 + start.Month) + 1;
-                for (var i = 1; i < diffMonth - 1; i++)
-                {
-                    var midAmount = budgets
-                                    .FirstOrDefault(j => j.YearMonth == start.AddMonths(i).ToString("yyyyMM"))
-                                    ?.Amount ?? 0;
-                    tmpMid += midAmount;
-                }
 
-                return s + tmpMid + e;
+            var s = (daysOfStartBudget - start.Day + 1) * dailyAmountOfStart;
+            var e = end.Day * dailyAmountOfEnd;
+            var tmpMid = (decimal) 0;
+            var diffMonth = end.Year * 12 + end.Month - (start.Year * 12 + start.Month) + 1;
+            for (var i = 1; i < diffMonth - 1; i++)
+            {
+                var midAmount = budgets
+                                .FirstOrDefault(j => j.YearMonth == start.AddMonths(i).ToString("yyyyMM"))
+                                ?.Amount ?? 0;
+                tmpMid += midAmount;
             }
 
-            return 0;
+            return s + tmpMid + e;
         }
     }
 }
