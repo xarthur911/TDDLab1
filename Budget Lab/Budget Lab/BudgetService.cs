9bc7a3aee97361fa1ab03e761718d2435d2fd138
Joey Chen <jay.hatelove@gmail.com>
2020-05-03 10:22:15 +0800

    [refactor]rename variable "budgets" and move variable to closest it used

--- Budget Lab/Budget Lab/BudgetService.cs
+++ Budget Lab/Budget Lab/BudgetService.cs
@@ -12,51 +12,52 @@ namespace Budget_Lab
         {
             this._budgetRepo = budgetRepo;
         }
+
         public decimal Query(DateTime start, DateTime end)
         {
-            var allAmount = this._budgetRepo.GetAll();
-            var diffDays = end.Subtract(start).TotalDays+1;
-            var diffMonth = end.Year * 12 + end.Month - (start.Year * 12 + start.Month) + 1;
-            var startMonthDays = DateTime.DaysInMonth(start.Year, start.Month);
-            var endMonthDays = DateTime.DaysInMonth(end.Year, end.Month);
-            var startAmount = allAmount
-                .FirstOrDefault(i => i.YearMonth == start.ToString("yyyyMM"))
-                ?.Amount ?? 0;
-            var endAmount = allAmount
-                .FirstOrDefault(i => i.YearMonth == end.ToString("yyyyMM"))
-                ?.Amount ?? 0;
+            var budgets = _budgetRepo.GetAll();
 
+            var startMonthDays = DateTime.DaysInMonth(start.Year, start.Month);
+            var startAmount = budgets
+                              .FirstOrDefault(i => i.YearMonth == start.ToString("yyyyMM"))
+                              ?.Amount ?? 0;
             decimal startOneDay = startAmount / startMonthDays;
+            
+            var endMonthDays = DateTime.DaysInMonth(end.Year, end.Month);
+            var endAmount = budgets
+                            .FirstOrDefault(i => i.YearMonth == end.ToString("yyyyMM"))
+                            ?.Amount ?? 0;
             decimal endOneDay = endAmount / endMonthDays;
 
-            //// end < start
-            if (diffDays < 1)
+            var intervalDays = end.Subtract(start).TotalDays + 1;
+            if (intervalDays < 1)
+                //// end < start
             {
                 return 0;
             }
 
             //// 當天
-            if (diffDays == 1)
+            if (intervalDays == 1)
             {
                 return startOneDay;
             }
 
-
-            if (diffMonth<2)
+            var diffMonth = end.Year * 12 + end.Month - (start.Year * 12 + start.Month) + 1;
+            if (diffMonth < 2)
             {
                 //// 當月超過1日
-                return (decimal)(diffDays) * startOneDay;
+                return (decimal) (intervalDays) * startOneDay;
             }
             else
             {
                 var s = (startMonthDays - start.Day + 1) * startOneDay;
                 var e = end.Day * endOneDay;
                 var tmpMid = (decimal) 0;
-                for (var i = 1; i < diffMonth-1; i++)
+                for (var i = 1; i < diffMonth - 1; i++)
                 {
-                    var midAmount = allAmount
-                        .FirstOrDefault(j => j.YearMonth == start.AddMonths(i).ToString("yyyyMM"))
-                        ?.Amount ?? 0;
+                    var midAmount = budgets
+                                    .FirstOrDefault(j => j.YearMonth == start.AddMonths(i).ToString("yyyyMM"))
+                                    ?.Amount ?? 0;
                     tmpMid += midAmount;
                 }
 
@@ -65,6 +66,5 @@ namespace Budget_Lab
 
             return 0;
         }
-
     }
 }
