599e2405b885f7a89ce846bce2f18cbdb42055cd
Joey Chen <jay.hatelove@gmail.com>
2020-05-03 10:26:47 +0800

    [refactor] rename, and fix int/int problem

--- Budget Lab/Budget Lab/BudgetService.cs
+++ Budget Lab/Budget Lab/BudgetService.cs
@@ -17,17 +17,17 @@ namespace Budget_Lab
         {
             var budgets = _budgetRepo.GetAll();
 
-            var startMonthDays = DateTime.DaysInMonth(start.Year, start.Month);
-            var startAmount = budgets
+            var daysOfStartBudget = DateTime.DaysInMonth(start.Year, start.Month);
+            var amountOfStartBudget = budgets
                               .FirstOrDefault(i => i.YearMonth == start.ToString("yyyyMM"))
                               ?.Amount ?? 0;
-            decimal startOneDay = startAmount / startMonthDays;
+            decimal dailyAmountOfStart = (decimal)amountOfStartBudget / daysOfStartBudget;
             
-            var endMonthDays = DateTime.DaysInMonth(end.Year, end.Month);
-            var endAmount = budgets
+            var daysOfEndBudget = DateTime.DaysInMonth(end.Year, end.Month);
+            var amountOfEndBudget = budgets
                             .FirstOrDefault(i => i.YearMonth == end.ToString("yyyyMM"))
                             ?.Amount ?? 0;
-            decimal endOneDay = endAmount / endMonthDays;
+            decimal dailyAmountOfEnd = (decimal)amountOfEndBudget / daysOfEndBudget;
 
             var intervalDays = end.Subtract(start).TotalDays + 1;
             if (intervalDays < 1)
@@ -39,19 +39,19 @@ namespace Budget_Lab
             //// 當天
             if (intervalDays == 1)
             {
-                return startOneDay;
+                return dailyAmountOfStart;
             }
 
             var diffMonth = end.Year * 12 + end.Month - (start.Year * 12 + start.Month) + 1;
             if (diffMonth < 2)
             {
                 //// 當月超過1日
-                return (decimal) (intervalDays) * startOneDay;
+                return (decimal) (intervalDays) * dailyAmountOfStart;
             }
             else
             {
-                var s = (startMonthDays - start.Day + 1) * startOneDay;
-                var e = end.Day * endOneDay;
+                var s = (daysOfStartBudget - start.Day + 1) * dailyAmountOfStart;
+                var e = end.Day * dailyAmountOfEnd;
                 var tmpMid = (decimal) 0;
                 for (var i = 1; i < diffMonth - 1; i++)
                 {
